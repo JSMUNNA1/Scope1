a5602afd2336cea4a3b5cd70539c14bd62bc93f5
Munna Thakur <134777241+JSMUNNA1@users.noreply.github.com>
2024-08-06 12:18:14 +0530

    Update Qdoc3.js

--- Qdoc3.js
+++ Qdoc3.js
@@ -2,4 +2,4 @@ const arr=[1,2,3,4,5,6];
 let arr2=[1,23]
 arr=arr2;
    
-   console.log(arr)
+   console.log(arr)//This code is gives the error because reassigning the variable to a different array.
