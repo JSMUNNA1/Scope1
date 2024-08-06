8ea6445b50e7d21d825c06904ab0210254b974d7
Munna Thakur <134777241+JSMUNNA1@users.noreply.github.com>
2024-08-06 12:18:52 +0530

    Update Qdoc5.js

--- Qdoc5.js
+++ Qdoc5.js
@@ -2,3 +2,8 @@
   let i=10;
 }
 console.log(i);
+//Output:
+ReferenceError: i is not defined
+
+
+Because the let variable is block-scoped variable that wise not accessible out side the block not allowing.
